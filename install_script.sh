2e494764b03223e80b11b5e1b856b12d735a30ad
Lachlan Douglas <lachlan@engines.org>
2016-10-23 10:02:52 +1100

    wrap install and uninstall scripts with function then call

--- install_script.sh
+++ install_script.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sudo apt-get -y update;
-sudo apt-get install -y git </dev/null;
-cd /tmp;
-git clone --depth=1 https://github.com/EnginesOS/EnginesInstaller;
-cd EnginesInstaller;
-sudo bash ./install_engines.sh;
+function install {
+  sudo apt-get -y update </dev/null
+  sudo apt-get install -y git </dev/null
+  cd /tmp;
+  git clone https://github.com/EnginesOS/EnginesInstaller;
+  cd EnginesInstaller;
+  sudo bash ./install_engines.sh;
+}
+install
--- uninstall_script.sh
+++ uninstall_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-cd /tmp;
-rm -rf EnginesInstaller;
-git clone --depth=1 https://github.com/EnginesOS/EnginesInstaller;
-sudo sh ./EnginesInstaller/obliterate_engines_no_warning.sh;
+function uninstall {
+  cd /tmp;
+  rm -rf EnginesInstaller;
+  git clone https://github.com/EnginesOS/EnginesInstaller;
+  sudo sh ./EnginesInstaller/obliterate_engines_no_warning.sh;
+}
+uninstall
